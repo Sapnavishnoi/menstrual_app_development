5160b02479664c77312f6a21845aa2dc6b86541a
Sapnavishnoi <sapna18@navgurukul.org>
2019-03-09 14:23:45 +0530

    ending date is greater than starting date #validation

--- app/models/menstrual_cycle.rb
+++ app/models/menstrual_cycle.rb
@@ -1,3 +1,13 @@
 class MenstrualCycle < ApplicationRecord
   belongs_to :user
+  validates :starting_date, presence: true
+  validates :ending_date, presence: true
+  validate :ending_date_is_greater_than_start_date
+  
+  def ending_date_is_greater_than_start_date
+    if ending_date.present? && starting_date > ending_date
+      errors.add(:ending_date, "ending date is less than starting date")
+    end
+  end
+
 end
