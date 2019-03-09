17c56d03fca18ef93d11f9ca684c2006d1b3c74b
Sapnavishnoi <sapna18@navgurukul.org>
2019-03-09 15:02:17 +0530

    add next cycle date

--- app/models/menstrual_cycle.rb
+++ app/models/menstrual_cycle.rb
@@ -1,9 +1,14 @@
 class MenstrualCycle < ApplicationRecord
   belongs_to :user
+
   validates :starting_date, presence: true
   validates :ending_date, presence: true
   validate :ending_date_is_greater_than_start_date
   
+  def next_predicted_date
+    starting_date + 28.days
+  end
+
   def ending_date_is_greater_than_start_date
     if ending_date.present? && starting_date > ending_date
       errors.add(:ending_date, "ending date is less than starting date")
